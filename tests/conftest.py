3f57375fad55caf643b4c2f274cfd077fe2b4832
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-05-20 13:33:26 -0700

    add fixture of SE and MXE splice types so they're auto-switched

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,9 @@ def mxe_miso_ids():
     return ['chr1:100:200:+@chr1:300:400:+@chr1:500:600:+@chr1:700:800:+',
             'chr2:700:800:-@chr2:500:600:-@chr2:300:400:-@chr2:100:200:-']
 
+@pytest.fixture(params=['SE', 'MXE'])
+def miso_ids_splice_type(request, se_miso_ids, mxe_miso_ids):
+    if request.param == 'SE':
+        return request, se_miso_ids
+    elif request.param == 'MXE':
+        return request, mxe_miso_ids
